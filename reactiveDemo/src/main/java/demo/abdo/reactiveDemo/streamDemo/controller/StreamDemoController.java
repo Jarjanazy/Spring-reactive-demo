6abb6214634211414d06545e132e569fa9914516
jarjanazy <celil.cercenazi@gmail.com>
2021-02-20 19:41:19 +0300

    Add a controller to demo the JSON stream using Flux

---
+++ reactiveDemo/src/main/java/demo/abdo/reactiveDemo/streamDemo/controller/StreamDemoController.java
@@ -0,0 +1,29 @@
+package demo.abdo.reactiveDemo.streamDemo.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+@RestController
+public class StreamDemoController {
+    /* since the default return type is JSON, it only returns when the four elements are all done, i.e. after 4 seconds */
+    @GetMapping("/integers")
+    public Flux<Integer> getIntegers(){
+        return Flux.
+                just(1, 2, 3, 4).
+                delayElements(Duration.ofSeconds(1));
+    }
+
+    /* this will return each element when ready, i.e. it will return one element on every second */
+    @GetMapping(value = "integers/stream", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<Integer> getIntegersStream(){
+        return Flux.
+                just(1, 2, 3, 4).
+                delayElements(Duration.ofSeconds(1));
+    }
+
+
+}
