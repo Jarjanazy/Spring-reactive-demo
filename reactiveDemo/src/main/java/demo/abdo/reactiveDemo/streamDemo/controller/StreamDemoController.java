c09a5bc90c64461621c3300c2271ddb844e9e226
jarjanazy <celil.cercenazi@gmail.com>
2021-02-21 11:19:12 +0300

    Add an endpoint for an infinite stream of Long values

--- reactiveDemo/src/main/java/demo/abdo/reactiveDemo/streamDemo/controller/StreamDemoController.java
+++ reactiveDemo/src/main/java/demo/abdo/reactiveDemo/streamDemo/controller/StreamDemoController.java
@@ -4,6 +4,8 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import java.util.Random;
+
 
 import java.time.Duration;
 
@@ -25,5 +27,9 @@ public class StreamDemoController {
                 delayElements(Duration.ofSeconds(1));
     }
 
-
+    @GetMapping(value = "integers/stream/infinite", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    public Flux<Long> getIntegersInfiniteStream(){
+        return Flux.
+                interval(Duration.ofSeconds(1));
+    }
 }
